5e91b1c36ff0bf47bb844729ca311cd72cb7abc1
Aeriel Denmark <aeriel.denmark@gmail.com>
2022-08-21 16:43:15 -0700

    Create dog.java

---
+++ dog.java
@@ -0,0 +1,39 @@
+// Aeriel Denmark
+// Foundation in Application Development
+
+// The following is a program written for an app development course taken towards Computer Science, B.S. degree
+// This file is one of several files that links to one another
+
+public class Dog extends RescueAnimal {
+
+    // Instance variable
+    private String breed;
+
+    // Constructor
+    public Dog(String name, String breed, String gender, String age,
+    String weight, String acquisitionDate, String acquisitionCountry,
+	String trainingStatus, boolean reserved, String inServiceCountry) {
+        setName(name);
+        setBreed(breed);
+        setGender(gender);
+        setAge(age);
+        setWeight(weight);
+        setAcquisitionDate(acquisitionDate);
+        setAcquisitionLocation(acquisitionCountry);
+        setTrainingStatus(trainingStatus);
+        setReserved(reserved);
+        setInServiceCountry(inServiceCountry);
+
+    }
+
+    // Accessor Method
+    public String getBreed() {
+        return breed;
+    }
+
+    // Mutator Method
+    public void setBreed(String dogBreed) {
+        breed = dogBreed;
+    }
+
+}
